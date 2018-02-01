8432561ffde39c0ebd42161fe313ae397386afb5
Sandhya Rao <smudra@yahoo.com>
2018-01-31 18:35:07 -0600

    Tried my best

--- main.js
+++ main.js
@@ -9,10 +9,16 @@ const CardMaker = {
   
   //Refactor this to link this object to cardMaker
   //const BirthdayCard = Object.create();
+const moreCards = {
+    childrensCard: "Happy Birthday, kiddo",
+    adultCard: "Happy Birthday, Mom"
+}
+
 
   const CardMaker = {
-      makeFrontMessage: function() {
+      makeFrontMessage: function(wish) {
           console.log(this.wish);
+        //return `<h2>${wish}</h2>`
       },
       makeInsideMessage: function() {
           console.log(this.inside);
@@ -23,16 +29,28 @@ const CardMaker = {
   const birthday = Object.create(CardMaker);
   birthday.wish = "Happy Birthday";
   birthday.makeFrontMessage();
+  birthday.inside = "Have a blast on your Birthday";
+  birthday.makeInsideMessage();
+
+
+  /*const birthdayInside = Object.create(CardMaker);
+  birthdayInside.inside = "Have a blast on your Birthday";
+  birthdayInside.makeInsideMessage();*/
+
+  const childBirthday = Object.create(birthday);
+  childBirthday.wish = "Happy Birthday, kiddo";
+  childBirthday.makeFrontMessage();
+
+  const adultBirthday = Object.create(birthday);
+  adultBirthday.wish = "Happy Birthday, Mom";
+  adultBirthday.makeFrontMessage();
 
   const anniversary = Object.create(CardMaker);
   anniversary.wish = "Happy Anniversary";
   anniversary.makeFrontMessage();
-  
+  anniversary.inside = "Happy times together on your Anniversary";
+  anniversary.makeInsideMessage();
 
-  const birthdayInside = Object.create(CardMaker);
-  birthdayInside.inside = "Have a blast on your Birthday";
-  birthdayInside.makeInsideMessage();
-
-  const anniversaryInside = Object.create(CardMaker);
-  anniversaryInside.inside = "Happy times together";
-  anniversaryInside.makeInsideMessage();
+  /*const anniversaryInside = Object.create(CardMaker);
+  anniversaryInside.inside = "Happy times together on your Anniversary";
+  anniversaryInside.makeInsideMessage(); */
